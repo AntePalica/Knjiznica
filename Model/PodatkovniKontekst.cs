788c010b60e29f02e56e022060a30411f8502104
AntePalica <ragacivek@gmail.com>
2025-10-15 11:54:16 +0200

    PodatkovniKontekst dodan

---
+++ Model/PodatkovniKontekst.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Knjiznica.Model
+{
+    public class PodatkovniKontekst
+    {
+        public List<Ucenik> ucenici;
+        public List<Knjiga> knjige;
+        public List<Posudba> posudbe;
+
+
+
+
+    }
+}
