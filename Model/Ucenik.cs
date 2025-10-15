2f7d7bc362cab490d6b104beff95fe9316626507
AntePalica <ragacivek@gmail.com>
2025-10-15 11:31:49 +0200

    Ucenik gotov

---
+++ Model/Ucenik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Knjiznica.Model
+{
+    public class Ucenik : IComparable 
+    {
+        public string OIB { get; set; }
+
+        public string Ime { get; set; }
+        
+        public string Prezime { get; set; }
+       
+        public string Adresa { get; set; }
+
+        public string Telefon { get; set; }
+        
+        public int Razred { get; set; }
+
+
+    }
+}
