a88dcbc8793884075ef360f1e6a68f18de038f6d
AntePalica <ragacivek@gmail.com>
2025-10-28 18:43:01 +0100

    posudbe forma gotova

---
+++ Forme/Posudbe.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Knjiznica.Forme
+{
+    partial class Posudbe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbUcenici = new System.Windows.Forms.ListBox();
+            this.lbKnjige = new System.Windows.Forms.ListBox();
+            this.dtpDatumPosudbe = new System.Windows.Forms.DateTimePicker();
+            this.cbBrojDana = new System.Windows.Forms.ComboBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbUcenici
+            // 
+            this.lbUcenici.FormattingEnabled = true;
+            this.lbUcenici.Location = new System.Drawing.Point(63, 26);
+            this.lbUcenici.Name = "lbUcenici";
+            this.lbUcenici.Size = new System.Drawing.Size(277, 147);
+            this.lbUcenici.TabIndex = 0;
+            this.lbUcenici.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lbKnjige
+            // 
+            this.lbKnjige.FormattingEnabled = true;
+            this.lbKnjige.Location = new System.Drawing.Point(415, 26);
+            this.lbKnjige.Name = "lbKnjige";
+            this.lbKnjige.Size = new System.Drawing.Size(277, 147);
+            this.lbKnjige.TabIndex = 1;
+            // 
+            // dtpDatumPosudbe
+            // 
+            this.dtpDatumPosudbe.Location = new System.Drawing.Point(63, 233);
+            this.dtpDatumPosudbe.Name = "dtpDatumPosudbe";
+            this.dtpDatumPosudbe.Size = new System.Drawing.Size(200, 20);
+            this.dtpDatumPosudbe.TabIndex = 2;
+            // 
+            // cbBrojDana
+            // 
+            this.cbBrojDana.FormattingEnabled = true;
+            this.cbBrojDana.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.cbBrojDana.Location = new System.Drawing.Point(415, 232);
+            this.cbBrojDana.Name = "cbBrojDana";
+            this.cbBrojDana.Size = new System.Drawing.Size(200, 21);
+            this.cbBrojDana.TabIndex = 3;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(326, 308);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(112, 79);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Učenik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(415, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Knjiga";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Datum Posudbe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(415, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Broj Dana";
+            // 
+            // Posudbe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.cbBrojDana);
+            this.Controls.Add(this.dtpDatumPosudbe);
+            this.Controls.Add(this.lbKnjige);
+            this.Controls.Add(this.lbUcenici);
+            this.Name = "Posudbe";
+            this.Text = "Posudbe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbUcenici;
+        private System.Windows.Forms.ListBox lbKnjige;
+        private System.Windows.Forms.DateTimePicker dtpDatumPosudbe;
+        private System.Windows.Forms.ComboBox cbBrojDana;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Forme/Posudbe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Knjiznica.Forme
+{
+    public partial class Posudbe : Form
+    {
+        public Posudbe()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
